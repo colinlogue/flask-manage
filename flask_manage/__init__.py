63bfd40173fa85b3565b59de27c921c4d873116e
Colin Logue <colinlogue@gmail.com>
2019-09-15 20:11:52 -0700

    fixed newproject command

--- flask_manage/__init__.py
+++ flask_manage/__init__.py
@@ -1,25 +1,25 @@
 from argparse import ArgumentParser
 from flask_manage.commands import newproject
 
-def main(argv=None):
+def main(argv):
     parser = ArgumentParser(
         prog='flask-manage',
         description='Create flask project template.')
-    subparsers = parser.add_subparsers(title="commands")
+    subparsers = parser.add_subparsers(title='commands')
 
     parser_newproject = subparsers.add_parser('newproject')
+    parser_newproject.add_argument(
+        'label',
+        help='name of the project to be created. used as the new directory name.')
     parser_newproject.add_argument(
         'dest',
         nargs='?',
         default='.',
         help='location to install project template (default current directory)')
-    parser_newproject.set_defaults(func=newproject)
+    parser_newproject.set_defaults(command=newproject)
 
-    if argv is not None:
-        args = parser.parse_args(argv)
-    else:
-        args = parser.parse_args()
-    args.func(args)
+    args = parser.parse_args(argv)
+    args.command.execute(args)
 
 def register_modules(app):
     pass
--- flask_manage/commands/newproject.py
+++ flask_manage/commands/newproject.py
@@ -1,7 +1,12 @@
 import os
 import shutil
 
-def newproject(args):
+PROJECT_TEMPLATE_PATH = 'flask_manage/project_template'
+
+def ignore_pycache(current_dir, contents):
+    return ['__pycache__']
+
+def execute(args):
     dest = os.path.join(args.dest, args.label)
-    src = os.path.join(os.path.dirname(__file__), 'project_template')
-    shutil.copytree(src, dest)
+    src = os.path.join(PROJECT_TEMPLATE_PATH)
+    shutil.copytree(src, dest, ignore=ignore_pycache)
